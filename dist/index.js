33cdd600264e0a20073f81fa52279da94cb061cf
sskyy <skyking_H@hotmail.com>
2025-02-18 21:09:43 +0800

    chore: update gitignore

--- dist/index.js
+++ dist/index.js
@@ -29,12 +29,12 @@ function R(r, e) {
 var B = "beforeunload", K = "hashchange", W = "popstate";
 function F(r) {
   r === void 0 && (r = {});
-  var e = r, a = e.window, n = a === void 0 ? document.defaultView : a, o = n.history;
-  function s() {
-    var c = n.location, t = c.pathname, i = c.search, d = c.hash, f = o.state || {};
+  var e = r, a = e.window, n = a === void 0 ? document.defaultView : a, i = n.history;
+  function o() {
+    var c = n.location, t = c.pathname, s = c.search, d = c.hash, f = i.state || {};
     return [f.idx, T({
       pathname: t,
-      search: i,
+      search: s,
       hash: d,
       state: f.usr || null,
       key: f.key || "default"
@@ -45,10 +45,10 @@ function F(r) {
     if (l)
       y.call(l), l = null;
     else {
-      var c = b.Pop, t = s(), i = t[0], d = t[1];
+      var c = b.Pop, t = o(), s = t[0], d = t[1];
       if (y.length)
-        if (i != null) {
-          var f = p - i;
+        if (s != null) {
+          var f = p - s;
           f && (l = {
             action: c,
             location: d,
@@ -65,12 +65,12 @@ function F(r) {
             "You are trying to block a POP navigation to a location that was not created by the history library. The block will fail silently in production, but in general you should do all navigation with the history library (instead of using window.history.pushState directly) to avoid this situation."
           );
       else
-        $(c);
+        _(c);
     }
   }
   n.addEventListener(W, E);
-  var w = b.Pop, N = s(), p = N[0], v = N[1], x = M(), y = M();
-  p == null && (p = 0, o.replaceState(C({}, o.state, {
+  var w = b.Pop, N = o(), p = N[0], v = N[1], x = M(), y = M();
+  p == null && (p = 0, i.replaceState(C({}, i.state, {
     idx: p
   }), ""));
   function L(c) {
@@ -86,55 +86,55 @@ function F(r) {
       key: U()
     }));
   }
-  function _(c, t) {
+  function $(c, t) {
     return [{
       usr: c.state,
       key: c.key,
       idx: t
     }, L(c)];
   }
-  function O(c, t, i) {
+  function O(c, t, s) {
     return !y.length || (y.call({
       action: c,
       location: t,
-      retry: i
+      retry: s
     }), !1);
   }
-  function $(c) {
+  function _(c) {
     w = c;
-    var t = s();
+    var t = o();
     p = t[0], v = t[1], x.call({
       action: w,
       location: v
     });
   }
   function h(c, t) {
-    var i = b.Push, d = S(c, t);
+    var s = b.Push, d = S(c, t);
     function f() {
       h(c, t);
     }
-    if (O(i, d, f)) {
-      var P = _(d, p + 1), k = P[0], H = P[1];
+    if (O(s, d, f)) {
+      var P = $(d, p + 1), k = P[0], H = P[1];
       try {
-        o.pushState(k, "", H);
+        i.pushState(k, "", H);
       } catch {
         n.location.assign(H);
       }
-      $(i);
+      _(s);
     }
   }
   function u(c, t) {
-    var i = b.Replace, d = S(c, t);
+    var s = b.Replace, d = S(c, t);
     function f() {
       u(c, t);
     }
-    if (O(i, d, f)) {
-      var P = _(d, p), k = P[0], H = P[1];
-      o.replaceState(k, "", H), $(i);
+    if (O(s, d, f)) {
+      var P = $(d, p), k = P[0], H = P[1];
+      i.replaceState(k, "", H), _(s);
     }
   }
   function m(c) {
-    o.go(c);
+    i.go(c);
   }
   var g = {
     get action() {
@@ -157,9 +157,9 @@ function F(r) {
       return x.push(t);
     },
     block: function(t) {
-      var i = y.push(t);
+      var s = y.push(t);
       return y.length === 1 && n.addEventListener(B, J), function() {
-        i(), y.length || n.removeEventListener(B, J);
+        s(), y.length || n.removeEventListener(B, J);
       };
     }
   };
@@ -167,9 +167,9 @@ function F(r) {
 }
 function X(r) {
   r === void 0 && (r = {});
-  var e = r, a = e.window, n = a === void 0 ? document.defaultView : a, o = n.history;
-  function s() {
-    var t = j(n.location.hash.substr(1)), i = t.pathname, d = i === void 0 ? "/" : i, f = t.search, P = f === void 0 ? "" : f, k = t.hash, H = k === void 0 ? "" : k, A = o.state || {};
+  var e = r, a = e.window, n = a === void 0 ? document.defaultView : a, i = n.history;
+  function o() {
+    var t = j(n.location.hash.substr(1)), s = t.pathname, d = s === void 0 ? "/" : s, f = t.search, P = f === void 0 ? "" : f, k = t.hash, H = k === void 0 ? "" : k, A = i.state || {};
     return [A.idx, T({
       pathname: d,
       search: P,
@@ -183,7 +183,7 @@ function X(r) {
     if (l)
       y.call(l), l = null;
     else {
-      var t = b.Pop, i = s(), d = i[0], f = i[1];
+      var t = b.Pop, s = o(), d = s[0], f = s[1];
       if (y.length)
         if (d != null) {
           var P = p - d;
@@ -207,83 +207,83 @@ function X(r) {
     }
   }
   n.addEventListener(W, E), n.addEventListener(K, function() {
-    var t = s(), i = t[1];
-    D(i) !== D(v) && E();
+    var t = o(), s = t[1];
+    D(s) !== D(v) && E();
   });
-  var w = b.Pop, N = s(), p = N[0], v = N[1], x = M(), y = M();
-  p == null && (p = 0, o.replaceState(C({}, o.state, {
+  var w = b.Pop, N = o(), p = N[0], v = N[1], x = M(), y = M();
+  p == null && (p = 0, i.replaceState(C({}, i.state, {
     idx: p
   }), ""));
   function L() {
-    var t = document.querySelector("base"), i = "";
+    var t = document.querySelector("base"), s = "";
     if (t && t.getAttribute("href")) {
       var d = n.location.href, f = d.indexOf("#");
-      i = f === -1 ? d : d.slice(0, f);
+      s = f === -1 ? d : d.slice(0, f);
     }
-    return i;
+    return s;
   }
   function S(t) {
     return L() + "#" + (typeof t == "string" ? t : D(t));
   }
-  function _(t, i) {
-    return i === void 0 && (i = null), T(C({
+  function $(t, s) {
+    return s === void 0 && (s = null), T(C({
       pathname: v.pathname,
       hash: "",
       search: ""
     }, typeof t == "string" ? j(t) : t, {
-      state: i,
+      state: s,
       key: U()
     }));
   }
-  function O(t, i) {
+  function O(t, s) {
     return [{
       usr: t.state,
       key: t.key,
-      idx: i
+      idx: s
     }, S(t)];
   }
-  function $(t, i, d) {
+  function _(t, s, d) {
     return !y.length || (y.call({
       action: t,
-      location: i,
+      location: s,
       retry: d
     }), !1);
   }
   function h(t) {
     w = t;
-    var i = s();
-    p = i[0], v = i[1], x.call({
+    var s = o();
+    p = s[0], v = s[1], x.call({
       action: w,
       location: v
     });
   }
-  function u(t, i) {
-    var d = b.Push, f = _(t, i);
+  function u(t, s) {
+    var d = b.Push, f = $(t, s);
     function P() {
-      u(t, i);
+      u(t, s);
     }
-    if (process.env.NODE_ENV !== "production" && R(f.pathname.charAt(0) === "/", "Relative pathnames are not supported in hash history.push(" + JSON.stringify(t) + ")"), $(d, f, P)) {
+    if (process.env.NODE_ENV !== "production" && R(f.pathname.charAt(0) === "/", "Relative pathnames are not supported in hash history.push(" + JSON.stringify(t) + ")"), _(d, f, P)) {
       var k = O(f, p + 1), H = k[0], A = k[1];
       try {
-        o.pushState(H, "", A);
+        i.pushState(H, "", A);
       } catch {
         n.location.assign(A);
       }
       h(d);
     }
   }
-  function m(t, i) {
-    var d = b.Replace, f = _(t, i);
+  function m(t, s) {
+    var d = b.Replace, f = $(t, s);
     function P() {
-      m(t, i);
+      m(t, s);
     }
-    if (process.env.NODE_ENV !== "production" && R(f.pathname.charAt(0) === "/", "Relative pathnames are not supported in hash history.replace(" + JSON.stringify(t) + ")"), $(d, f, P)) {
+    if (process.env.NODE_ENV !== "production" && R(f.pathname.charAt(0) === "/", "Relative pathnames are not supported in hash history.replace(" + JSON.stringify(t) + ")"), _(d, f, P)) {
       var k = O(f, p), H = k[0], A = k[1];
-      o.replaceState(H, "", A), h(d);
+      i.replaceState(H, "", A), h(d);
     }
   }
   function g(t) {
-    o.go(t);
+    i.go(t);
   }
   var c = {
     get action() {
@@ -302,11 +302,11 @@ function X(r) {
     forward: function() {
       g(1);
     },
-    listen: function(i) {
-      return x.push(i);
+    listen: function(s) {
+      return x.push(s);
     },
-    block: function(i) {
-      var d = y.push(i);
+    block: function(s) {
+      var d = y.push(s);
       return y.length === 1 && n.addEventListener(B, J), function() {
         d(), y.length || n.removeEventListener(B, J);
       };
@@ -316,7 +316,7 @@ function X(r) {
 }
 function Z(r) {
   r === void 0 && (r = {});
-  var e = r, a = e.initialEntries, n = a === void 0 ? ["/"] : a, o = e.initialIndex, s = n.map(function(h) {
+  var e = r, a = e.initialEntries, n = a === void 0 ? ["/"] : a, i = e.initialIndex, o = n.map(function(h) {
     var u = T(C({
       pathname: "/",
       search: "",
@@ -325,7 +325,7 @@ function Z(r) {
       key: U()
     }, typeof h == "string" ? j(h) : h));
     return process.env.NODE_ENV !== "production" && R(u.pathname.charAt(0) === "/", "Relative pathnames are not supported in createMemoryHistory({ initialEntries }) (invalid entry: " + JSON.stringify(h) + ")"), u;
-  }), l = z(o ?? s.length - 1, 0, s.length - 1), E = b.Pop, w = s[l], N = M(), p = M();
+  }), l = z(i ?? o.length - 1, 0, o.length - 1), E = b.Pop, w = o[l], N = M(), p = M();
   function v(h) {
     return typeof h == "string" ? h : D(h);
   }
@@ -357,23 +357,23 @@ function Z(r) {
     function c() {
       S(h, u);
     }
-    process.env.NODE_ENV !== "production" && R(w.pathname.charAt(0) === "/", "Relative pathnames are not supported in memory history.push(" + JSON.stringify(h) + ")"), y(m, g, c) && (l += 1, s.splice(l, s.length, g), L(m, g));
+    process.env.NODE_ENV !== "production" && R(w.pathname.charAt(0) === "/", "Relative pathnames are not supported in memory history.push(" + JSON.stringify(h) + ")"), y(m, g, c) && (l += 1, o.splice(l, o.length, g), L(m, g));
   }
-  function _(h, u) {
+  function $(h, u) {
     var m = b.Replace, g = x(h, u);
     function c() {
-      _(h, u);
+      $(h, u);
     }
-    process.env.NODE_ENV !== "production" && R(w.pathname.charAt(0) === "/", "Relative pathnames are not supported in memory history.replace(" + JSON.stringify(h) + ")"), y(m, g, c) && (s[l] = g, L(m, g));
+    process.env.NODE_ENV !== "production" && R(w.pathname.charAt(0) === "/", "Relative pathnames are not supported in memory history.replace(" + JSON.stringify(h) + ")"), y(m, g, c) && (o[l] = g, L(m, g));
   }
   function O(h) {
-    var u = z(l + h, 0, s.length - 1), m = b.Pop, g = s[u];
+    var u = z(l + h, 0, o.length - 1), m = b.Pop, g = o[u];
     function c() {
       O(h);
     }
     y(m, g, c) && (l = u, L(m, g));
   }
-  var $ = {
+  var _ = {
     get index() {
       return l;
     },
@@ -385,7 +385,7 @@ function Z(r) {
     },
     createHref: v,
     push: S,
-    replace: _,
+    replace: $,
     go: O,
     back: function() {
       O(-1);
@@ -400,7 +400,7 @@ function Z(r) {
       return p.push(u);
     }
   };
-  return $;
+  return _;
 }
 function z(r, e, a) {
   return Math.min(Math.max(r, e), a);
@@ -432,8 +432,8 @@ function U() {
   return Math.random().toString(36).substr(2, 8);
 }
 function D(r) {
-  var e = r.pathname, a = e === void 0 ? "/" : e, n = r.search, o = n === void 0 ? "" : n, s = r.hash, l = s === void 0 ? "" : s;
-  return o && o !== "?" && (a += o.charAt(0) === "?" ? o : "?" + o), l && l !== "#" && (a += l.charAt(0) === "#" ? l : "#" + l), a;
+  var e = r.pathname, a = e === void 0 ? "/" : e, n = r.search, i = n === void 0 ? "" : n, o = r.hash, l = o === void 0 ? "" : o;
+  return i && i !== "?" && (a += i.charAt(0) === "?" ? i : "?" + i), l && l !== "#" && (a += l.charAt(0) === "#" ? l : "#" + l), a;
 }
 function j(r) {
   var e = {};
@@ -446,20 +446,20 @@ function j(r) {
   return e;
 }
 class Y extends q {
-  constructor(e, a = F(), n = "", o) {
-    super(), this.data = e, this.history = a, this.parentPath = n, this.parent = o, this.pathname = V(""), this.handler = V(void 0), this.params = V({}), this.searchParams = V({}), this.path = V(""), this.children = new I([]), this.map = {
+  constructor(e, a = F(), n = "", i) {
+    super(), this.data = e, this.history = a, this.parentPath = n, this.parent = i, this.pathname = V(""), this.handler = V(void 0), this.params = V({}), this.searchParams = V({}), this.path = V(""), this.children = new I([]), this.map = {
       children: /* @__PURE__ */ new Map(),
       handler: void 0,
       redirect: void 0,
       strictHandler: void 0
     }, this.onHistoryChange = () => {
       this.pathname(this.history.location.pathname);
-      const s = this.recognize(this.history.location.pathname);
-      if (this.handler(s.handler), this.path(s.path), this.params(s.params), this.searchParams(s.searchParams), s.redirect && s.isExactMatch) {
-        const [l, E] = Array.isArray(s.redirect) ? s.redirect : [s.redirect, !1];
+      const o = this.recognize(this.history.location.pathname);
+      if (this.handler(o.handler), this.path(o.path), this.params(o.params), this.searchParams(o.searchParams), o.redirect && o.isExactMatch) {
+        const [l, E] = Array.isArray(o.redirect) ? o.redirect : [o.redirect, !1];
         this.push(l, E);
       }
-    }, e.forEach((s) => this.addOne(s)), this.unlisten = this.history.listen(this.onHistoryChange), this.onHistoryChange(), this.parent && this.parent.children.add(this);
+    }, e.forEach((o) => this.addOne(o)), this.unlisten = this.history.listen(this.onHistoryChange), this.onHistoryChange(), this.parent && this.parent.children.add(this);
   }
   destroy() {
     var e;
@@ -468,12 +468,12 @@ class Y extends q {
   addOne(e) {
     const a = e.path.split("/").filter(Boolean);
     let n = this.map;
-    a.forEach((o) => {
-      if (o.startsWith(":")) {
+    a.forEach((i) => {
+      if (i.startsWith(":")) {
         if (n.dynamicChildren)
           throw new Error("only one dynamic param is allowed");
         n.dynamicChildren = {
-          paramName: o.slice(1),
+          paramName: i.slice(1),
           node: {
             children: /* @__PURE__ */ new Map(),
             handler: void 0,
@@ -481,9 +481,9 @@ class Y extends q {
           }
         }, n = n.dynamicChildren.node;
       } else {
-        const s = n.children;
-        n = s.get(o), n || s.set(
-          o,
+        const o = n.children;
+        n = o.get(i), n || o.set(
+          i,
           n = {
             children: /* @__PURE__ */ new Map(),
             handler: void 0,
@@ -498,15 +498,15 @@ class Y extends q {
   }
   recognize(e) {
     const n = (e.startsWith(this.parentPath) ? e.slice(this.parentPath.length) : e).split("/").filter(Boolean);
-    let o = this.map;
-    const s = [], l = [], E = {}, w = {};
+    let i = this.map;
+    const o = [], l = [], E = {}, w = {};
     let N = !0;
     for (let v of n) {
-      const x = o.children.get(v);
+      const x = i.children.get(v);
       if (x)
-        s.push(v), l.push(v), o = x;
-      else if (o.dynamicChildren)
-        E[o.dynamicChildren.paramName] = v, s.push(o.dynamicChildren.paramName), l.push(v), o = o.dynamicChildren.node;
+        o.push(v), l.push(v), i = x;
+      else if (i.dynamicChildren)
+        E[i.dynamicChildren.paramName] = v, o.push(i.dynamicChildren.paramName), l.push(v), i = i.dynamicChildren.node;
       else {
         N = !1;
         break;
@@ -516,12 +516,12 @@ class Y extends q {
     for (const [v, x] of p)
       w[v] = x;
     return {
-      handler: o.handler || o.strictHandler,
-      redirect: o.redirect,
+      handler: i.handler || i.strictHandler,
+      redirect: i.redirect,
       params: E,
       searchParams: w,
       isExactMatch: N,
-      path: `/${s.join("/")}`,
+      path: `/${o.join("/")}`,
       matchPath: `/${l.join("/")}`
     };
   }
@@ -535,12 +535,17 @@ class Y extends q {
   }
   updateSearchParams(e) {
     const a = new URL(window.location.href), n = { ...this.searchParams.raw };
-    Object.entries(e).forEach(([o, s]) => {
-      s === void 0 ? (a.searchParams.delete(o), delete n[o]) : (a.searchParams.set(o, s), n[o] = s);
+    Object.entries(e).forEach(([i, o]) => {
+      o === void 0 ? (a.searchParams.delete(i), delete n[i]) : (a.searchParams.set(i, o), n[i] = o);
     }), window.history.pushState({}, "", a), this.searchParams(n);
   }
-  derive(e = this.path(), a = []) {
-    return new Y(a, this.history, `${this.parentPath}${e}`, this);
+  derive(e = this.path.raw) {
+    const a = this, n = `${a.parentPath}${e}`;
+    return class extends Y {
+      constructor(o, l = "") {
+        super(o, a.history, `${n}${l}`, a), this.data = o, this.subParentPath = l;
+      }
+    };
   }
 }
 export {
